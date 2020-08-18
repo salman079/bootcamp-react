5ceefc9253847a857ebc73103d92e3d23424a30b
salmansiddiqui <salman079@gmail.com>
2020-08-18 21:31:57 +0300

    https://github.com/salman079/bootcamp-react.git

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Saloo bhai 2nd update</p>
+        <p>Saloo bhai 3rd update</p>
         <a
           className="App-link"
           href="https://reactjs.org"
