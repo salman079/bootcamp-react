5436806da3f19102cd276b4848c1df1f8f5db623
salmansiddiqui <salman079@gmail.com>
2020-08-21 19:50:16 +0300

    theme

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   
   return (
     <div className="App">
+      <h1 style = {{color: `purple`}}> this is testing </h1> 
       <Text theme1={theme}/>
       <button onClick = {onClickHandler}>Change theme</button>
     </div>
@@ -17,7 +18,7 @@ function App() {
 }
 function Text({theme1}) {
   return(
-    <h1 style = {{color: `${theme1}`}}>{theme1}</h1>
+    <h1 style = {{color: `${theme1}`}}>Text in {theme1} color </h1>
   );
   }
 export default App;
